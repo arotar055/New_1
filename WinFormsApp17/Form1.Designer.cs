c3d0fbc41e47b83bbc961a76e95dd231a16b56f4
Rotar_mg75@student.itstep.org <Rotar_mg75@student.itstep.org>
2025-02-13 12:55:55 +0100

    Добавьте файлы проекта.

---
+++ WinFormsApp17/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WinFormsApp17
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+
+            // checkBox1
+            this.checkBox1.Location = new System.Drawing.Point(30, 20);
+            this.checkBox1.Text = "Запустить 1-й поток";
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+
+            // checkBox2
+            this.checkBox2.Location = new System.Drawing.Point(30, 50);
+            this.checkBox2.Text = "Приостановить 1-й поток";
+            this.checkBox2.Enabled = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+
+            // checkBox4
+            this.checkBox4.Location = new System.Drawing.Point(30, 90);
+            this.checkBox4.Text = "Запустить 2-й поток";
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+
+            // checkBox3
+            this.checkBox3.Location = new System.Drawing.Point(30, 120);
+            this.checkBox3.Text = "Приостановить 2-й поток";
+            this.checkBox3.Enabled = false;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+
+            // checkBox6
+            this.checkBox6.Location = new System.Drawing.Point(30, 160);
+            this.checkBox6.Text = "Запустить 3-й поток";
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+
+            // checkBox5
+            this.checkBox5.Location = new System.Drawing.Point(30, 190);
+            this.checkBox5.Text = "Приостановить 3-й поток";
+            this.checkBox5.Enabled = false;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+
+            // progressBar1
+            this.progressBar1.Location = new System.Drawing.Point(200, 30);
+            this.progressBar1.Size = new System.Drawing.Size(200, 20);
+
+            // progressBar2
+            this.progressBar2.Location = new System.Drawing.Point(200, 100);
+            this.progressBar2.Size = new System.Drawing.Size(200, 20);
+
+            // progressBar3
+            this.progressBar3.Location = new System.Drawing.Point(200, 170);
+            this.progressBar3.Size = new System.Drawing.Size(200, 20);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(450, 250);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar3);
+            this.Text = "Многопоточное приложение";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+    }
+}
---
+++ WinFormsApp17/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormsApp17
+{
+    public partial class Form1 : Form
+    {
+        private ManualResetEvent event_for_suspend1;
+        private ManualResetEvent event_for_stop1;
+        private Thread firstThread;
+
+        private ManualResetEvent event_for_suspend2;
+        private ManualResetEvent event_for_stop2;
+        private Thread secondThread;
+
+        private ManualResetEvent event_for_suspend3;
+        private ManualResetEvent event_for_stop3;
+        private Thread thirdThread;
+
+        public SynchronizationContext uiContext;
+        private Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            uiContext = SynchronizationContext.Current;
+            event_for_suspend1 = new ManualResetEvent(true);
+            event_for_stop1 = new ManualResetEvent(false);
+            event_for_suspend2 = new ManualResetEvent(true);
+            event_for_stop2 = new ManualResetEvent(false);
+            event_for_suspend3 = new ManualResetEvent(true);
+            event_for_stop3 = new ManualResetEvent(false);
+            random = new Random();
+        }
+
+        private void ThreadFunc(ProgressBar progressBar, ManualResetEvent suspendEvent, ManualResetEvent stopEvent)
+        {
+            uiContext.Send(d => progressBar.Minimum = 0, null);
+            uiContext.Send(d => progressBar.Maximum = 230, null);
+            uiContext.Send(d => progressBar.Value = 0, null);
+
+            while (true)
+            {
+                if (stopEvent.WaitOne(0)) break;
+
+                if (suspendEvent.WaitOne(0))
+                {
+                    int randomValue = random.Next(0, 231);
+                    uiContext.Send(d => progressBar.Value = randomValue, null);
+                }
+                Thread.Sleep(100);
+            }
+            uiContext.Send(d => progressBar.Value = 0, null);
+        }
+
+        private void StartThread(ref Thread thread, ProgressBar progressBar, ManualResetEvent suspendEvent, ManualResetEvent stopEvent)
+        {
+            stopEvent.Reset();
+            if (thread == null || !thread.IsAlive)
+            {
+                thread = new Thread(() => ThreadFunc(progressBar, suspendEvent, stopEvent))
+                {
+                    IsBackground = true
+                };
+                thread.Start();
+            }
+        }
+
+        private void StopThread(ManualResetEvent stopEvent, ManualResetEvent suspendEvent)
+        {
+            stopEvent.Set();
+            suspendEvent.Set();
+        }
+
+        private void ToggleThread(CheckBox startCheckBox, CheckBox suspendCheckBox, ref Thread thread, ProgressBar progressBar, ManualResetEvent suspendEvent, ManualResetEvent stopEvent)
+        {
+            if (startCheckBox.Checked)
+            {
+                StartThread(ref thread, progressBar, suspendEvent, stopEvent);
+                startCheckBox.Text = "Остановить";
+                suspendCheckBox.Enabled = true;
+            }
+            else
+            {
+                StopThread(stopEvent, suspendEvent);
+                startCheckBox.Text = "Запустить";
+                suspendCheckBox.Checked = false;
+                suspendCheckBox.Enabled = false;
+            }
+        }
+
+        private void ToggleSuspend(CheckBox suspendCheckBox, ManualResetEvent suspendEvent)
+        {
+            if (suspendCheckBox.Checked)
+            {
+                suspendEvent.Reset();
+                suspendCheckBox.Text = "Возобновить";
+            }
+            else
+            {
+                suspendEvent.Set();
+                suspendCheckBox.Text = "Приостановить";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) => ToggleThread(checkBox1, checkBox2, ref firstThread, progressBar1, event_for_suspend1, event_for_stop1);
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) => ToggleSuspend(checkBox2, event_for_suspend1);
+        private void checkBox4_CheckedChanged(object sender, EventArgs e) => ToggleThread(checkBox4, checkBox3, ref secondThread, progressBar2, event_for_suspend2, event_for_stop2);
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) => ToggleSuspend(checkBox3, event_for_suspend2);
+        private void checkBox6_CheckedChanged(object sender, EventArgs e) => ToggleThread(checkBox6, checkBox5, ref thirdThread, progressBar3, event_for_suspend3, event_for_stop3);
+        private void checkBox5_CheckedChanged(object sender, EventArgs e) => ToggleSuspend(checkBox5, event_for_suspend3);
+    }
+}
